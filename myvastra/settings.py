4e6a67d4f307265c083ef93718a64ef114f1618e
nitesh2910 <goadnitesh4813@gmail.com>
2025-07-31 17:37:31 +0530

    update : static_url

--- myvastra/settings.py
+++ myvastra/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
