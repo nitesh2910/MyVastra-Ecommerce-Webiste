703325c2fdcf72ccc4de2acf00987fd8c5f166b1
nitesh2910 <goadnitesh4813@gmail.com>
2025-07-31 11:05:26 +0530

    "Add Render domain to ALLOWED_HOSTS"

--- myvastra/settings.py
+++ myvastra/settings.py
@@ -160,3 +160,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ALLOWED_HOSTS = ['myvastra-ecommerce-webiste.onrender.com', 'localhost', '127.0.0.1']
