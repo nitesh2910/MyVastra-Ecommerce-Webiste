f739879845b91aa356260180b58f88c74a1bd7ec
nitesh2910 <goadnitesh4813@gmail.com>
2025-07-31 14:45:59 +0530

    "Add Render domain to ALLOWED_HOSTS"

--- myvastra/settings.py
+++ myvastra/settings.py
@@ -26,6 +26,8 @@ SECRET_KEY = 'django-insecure-+qhnh&zr*%@6rmkq7d6!2e0df_b2c-pq!y!d=@dqd++n-w+2m$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ALLOWED_HOSTS = []
+
 
 # Application definition
 
@@ -36,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'products',
     'category',
     'cart',
@@ -152,11 +154,10 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+ALLOWED_HOSTS = ['myvastra-ecommerce-webiste.onrender.com', 'localhost', '127.0.0.1']
